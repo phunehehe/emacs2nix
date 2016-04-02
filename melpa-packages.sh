62aa3396fab1fa4648bba1d6272d9d2f0bf9f28d
Hoang Xuan Phu <phuhimself@phunehehe.net>
2016-04-02 18:53:39 +0800

    update usage

--- melpa-packages.sh
+++ melpa-packages.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env nix-shell
 #!nix-shell -i bash -A env
 
-# usage: ./melpa-packages.sh --melpa PATH_TO_MELPA_CLONE
+# usage: ./melpa-packages.sh --melpa /path/to/melpa --output /path/to/output.json
 
 cabal run melpa2nix -- --work /tmp/melpa2nix "$@"
