94ce4673495c3f40cd27fd73c3c4ebff2d503a92
Hoang Xuan Phu <phuhimself@phunehehe.net>
2016-04-02 18:44:49 +0800

    no need for package overrides (anymore?)

--- shell.nix
+++ shell.nix
@@ -1,25 +1,5 @@
 with (import <nixpkgs> {});
-let
-  inherit (pkgs.haskell) lib;
-  haskellPackages = pkgs.haskell.packages.ghc801.override {
-    overrides = self: super: {
-      aeson = self.callPackage ./aeson.nix {};
-      comonad = lib.doJailbreak (lib.dontCheck super.comonad);
-      distributive = lib.dontCheck super.distributive;
-      fail = lib.dontHaddock super.fail;
-      hnix = self.callPackage ./hnix {};
-      kan-extensions = lib.doJailbreak (self.callPackage ./kan-extensions.nix {});
-      lens = lib.dontCheck (lib.doJailbreak (self.callPackage ./lens.nix {}));
-      parsers = lib.doJailbreak super.parsers;
-      reducers = lib.doJailbreak super.reducers;
-      semigroupoids = lib.dontCheck super.semigroupoids;
-      trifecta = lib.doJailbreak (lib.dontCheck super.trifecta);
-      unordered-containers =
-        lib.doJailbreak super.unordered-containers;
-    };
-  };
-in
-lib.addBuildTools
+haskell.lib.addBuildTools
   (haskellPackages.callPackage ./. {})
   [
     nix-prefetch-scripts git subversion cvs mercurial
